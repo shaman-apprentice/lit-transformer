0207d8f79585ee744a4c1e48f8ec80f41eafe444
torsten.knauf <torsten.knauf@omikron.net>
2018-12-29 11:45:17 +0100

    first test with jest

--- src/test/simpleDataBinding.test.js
+++ src/test/simpleDataBinding.test.js
@@ -1,5 +1,33 @@
-describe('simple data bindings', () => {
-  it('no bindings at all', () => {
+import { render } from 'lit-html'
+import Mustache from 'mustache'
 
+import transform from '../index'
+
+describe('data bindings', () => {
+  it('no bindings at all', () => {
+    const template = 'Hello world'
+    expect(stripLitExpressionMarkers(renderLit(template).innerHTML)).toBe(renderMustache(template).innerHTML)
+  })
+
+  it('simple string data binding', () => {
+    const template = 'Hello {{name}}!'
+    const data = { name: 'underworld' }
+    expect(stripLitExpressionMarkers(renderLit(template, data).innerHTML)).toBe(renderMustache(template, data).innerHTML)
   })
 })
+
+function renderMustache(template, data) {
+  const container = document.createElement('div')
+  container.innerHTML = Mustache.render(template, data)
+  return container
+}
+
+function renderLit(template, data) {
+  const container = document.createElement('div')
+  render(transform(template)(data), container)
+  return container
+}
+
+function stripLitExpressionMarkers (s) {
+  return s.replace(/<!---->/g, '')
+}

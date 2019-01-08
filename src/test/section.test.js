1e695e327f0c0eae2787d85892c741ee9ec7df64
Torsten.Knauf@gmx.de <Torsten.Knauf@gmx.de>
2019-01-08 20:51:15 +0100

    add tests to section

--- src/test/section.test.js
+++ src/test/section.test.js
@@ -8,8 +8,21 @@ describe('data bindings', () => {
     expectLitToBeMustache(template, data)
   })
 
-  it('PoC for section with List', () => {
+  it('section with list of strings', () => {
     const template = 'Hello {{#who}}{{.}}{{/who}}!'
     const data = { who: ['Dr.', ' Who'] }
-    expectLitToBeMustache(template, data)  })
+    expectLitToBeMustache(template, data)
+  })
+
+  it('section with list of data without html in section template', () => {
+    const template = '<ul>{{#persons}}{{person}}{{/persons}}</ul>'
+    const data = { persons: [ { person: 'Mr. first' }, { person: 'Mr. second' } ] }
+    expectLitToBeMustache(template, data)
+  })
+
+  it('section with list of data with html in section template', () => {
+    const template = '<ul>{{#persons}}<li>{{person}}</li>{{/persons}}</ul>'
+    const data = { persons: [ { person: 'Mr. first' }, { person: 'Mr. second' } ] }
+    expectLitToBeMustache(template, data)
+  })
 })

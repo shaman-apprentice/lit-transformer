0241fc32e7569512d903702e1ce4b83f70c94004
Torsten.Knauf@gmx.de <Torsten.Knauf@gmx.de>
2019-03-03 17:02:39 +0100

    simplify unsafeVariable test

--- src/transformers/unsafeVariable.js
+++ src/transformers/unsafeVariable.js
@@ -4,8 +4,7 @@ import { data2Value } from '../helper/dataHelper'
  * Note, this is unsafe to use cause the rendered output could be any JavaScript .
  */
 export default unsafeHTML => ({
-  test: (remainingTmplStr, { delimiter }) =>
-    remainingTmplStr[0] === '{' && remainingTmplStr.indexOf('}' + delimiter.end) >= 0,
+  test: remainingTmplStr => remainingTmplStr[0] === '{',
   transform: (remainingTmplStr, { delimiter }) => {
     const indexOfEndDelimiter = remainingTmplStr.indexOf('}' + delimiter.end)
     const dataKey = remainingTmplStr.substring(1, indexOfEndDelimiter)

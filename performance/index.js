0619002c2cdbb7682ac1020c0b6a923f9771afb5
shaman-apprentice <Torsten.Knauf@gmx.de>
2019-03-24 16:06:26 +0100

    add a static server to performance test
    for being independent of vs code's live server

--- performance/index.js
+++ performance/index.js
@@ -1,13 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
 const { measure } = require('./puppeteer');
 
 (async () => {
-  const mustacheData = await measure('http://127.0.0.1:5500/performance/mustache.html')
-  const litTransformerData = await measure('http://127.0.0.1:5500/performance/lit-transformer.html')
+  const server = createStupidServer()
+  server.listen(4242)
+
+  const mustacheData = await measure('http://127.0.0.1:4242/performance/mustache.html')
+  const litTransformerData = await measure('http://127.0.0.1:4242/performance/lit-transformer.html')
 
   const diff = Object.keys(litTransformerData).reduce((acc, key) => {
     acc[key] = mustacheData[key] - litTransformerData[key]
     return acc
   }, {})
-
   console.log(diff)
+  
+  server.close();
 })()
+
+function createStupidServer() {
+  return http.createServer((request, response) => {
+    if (request.url.endsWith('favicon.ico')) {
+      response.statusCode = 404
+      response.end()
+      return
+    }
+  
+    fs.readFile(`${__dirname}/../${request.url}`, (err, data) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+  
+      response.setHeader('Content-type', 'html')
+      response.end(data)
+    })
+  })
+}
